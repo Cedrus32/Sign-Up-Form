7c49b92eddefd06786d8a4af55dcfb028fe8819a
Shannon <srosenb@protonmail.ch>
2022-03-01 04:18:48 -0600

    Tweak password validation styling & bugs

--- script.js
+++ script.js
@@ -3,59 +3,54 @@
 // -------------- //
 
 function checkForm() {
-    checkName();
+    // checkName();
     // checkContact();
     checkPassword();
 }
 
-function checkName() {
-    // get variables
-    let fullName = document.querySelectorAll('fieldset.name input');
-    let fullNameErrorBoxes = document.querySelectorAll('fieldset.name div.error-box');
-    const alpha = /^[A-Z][a-z '-]?[a-z]{1,30}$/;
+// function checkName() {
+//     // get values
+//     let fullName = document.querySelectorAll('fieldset.name input');
+//     let fullNameErrorBoxes = document.querySelectorAll('fieldset.name div.error-box');
+//     const alpha = /^[A-Z][a-z '-]?[a-z]{1,30}$/;
 
-    // clear previous error messages
-    for (let i = 0; i <= 1; i++) {
-        let name = fullName[i];
-        let nameError = fullNameErrorBoxes[i];
-        name.classList = '';
-        nameError.textContent = '';
-    }
+//     // clear previous error messages
+//     for (let i = 0; i <= 1; i++) {
+//         let name = fullName[i];
+//         let nameError = fullNameErrorBoxes[i];
+//         name.classList = '';
+//         nameError.textContent = '';
+//     }
 
-    // check values
-    for (let i = 0; i <= 1; i++) {
-        let name = fullName[i];
-        let nameValue = name.value;
-        let nameTest = alpha.test(nameValue);
-        let nameError = fullNameErrorBoxes[i];
+//     // check values
+//     for (let i = 0; i <= 1; i++) {
+//         let name = fullName[i];
+//         let nameValue = name.value;
+//         let nameTest = alpha.test(nameValue);
+//         let nameError = fullNameErrorBoxes[i];
 
-        if (nameValue === '') {
-            name.classList.add('error');
-            if (i === 0) {
-                nameError.textContent = '* Please Enter Your First Name';
-            } else if (i === 1) {
-                nameError.textContent = '* Please Enter Your Last Name';
-            }
-            return false;
-        } else if (nameTest === false) {
-            name.classList.add('error');
-            if (i === 0) {
-                nameError.textContent = '* Please Enter Your First Name';
-            } else if (i === 1) {
-                nameError.textContent = '* Please Enter Your Last Name';
-            }
-            return false;
-        } else if (nameTest === true) {
-            name.classList.add('correct');
-            return true;
-        }
-    }
-}
-
-// checkContact
+//         if (nameValue === '') {
+//             name.classList.add('error');
+//             if (i === 0) {
+//                 nameError.textContent = '* Please Enter Your First Name';
+//             } else if (i === 1) {
+//                 nameError.textContent = '* Please Enter Your Last Name';
+//             }
+//         } else if (nameTest === false) {
+//             name.classList.add('error');
+//             if (i === 0) {
+//                 nameError.textContent = '* Please Enter Your First Name';
+//             } else if (i === 1) {
+//                 nameError.textContent = '* Please Enter Your Last Name';
+//             }
+//         } else if (nameTest === true) {
+//             name.classList.add('correct');
+//         }
+//     }
+// }
 
 function checkPassword () {
-    // get variables
+    // get values
     let pw1 = document.getElementById('pw');
     let pw2 = document.getElementById('pw-confirm');
     let pw1Value = pw1.value;
@@ -63,29 +58,35 @@ function checkPassword () {
     const pw1ErrorBox = document.getElementById('pw-error');
     const pw2ErrorBox = document.getElementById('pw-confirm-error');
 
+    console.log({pw1Value});
+    console.log({pw2Value});
+
     // clear previous error messages
-    pw1.classList.remove('error');
-    pw2.classList.remove('error');
+    pw1.classList = '';
+    pw2.classList = '';
     pw1ErrorBox.textContent = '';
     pw2ErrorBox.textContent = '';
 
-    // compare variables
+    // compare values
     switch (true) {
         case (pw1Value === ''):
             pw1.classList.add('error');
+            pw2.classList.add('error');
             pw1ErrorBox.textContent = '* Please Enter Password'
             break;
         case (pw2Value === ''):
             pw2.classList.add('error');
             pw2ErrorBox.textContent = '* Please Confirm Password'
             break;
-        case (pw1Value !== pw2Value):
+        case ((pw1Value !== pw2Value) && (pw1Value !== '') && (pw2Value !== '')):
             pw1.classList.add('error');
             pw2.classList.add('error');
-            pw1ErrorBox.textContent = '* Password did not match. Please confirm your password.'
-            return false;
-        case ((pw1 === pw2) && (pw1 !== '') && (pw2 !== '')):
-            return true;
+            pw1ErrorBox.textContent = '* Password did not match. Please confirm your password.';
+            break;
+        case ((pw1Value === pw2Value) && (pw1Value !== '') && (pw2Value !== '')):
+            console.log('correct');
+            pw1.classList.add('correct');
+            pw2.classList.add('correct');
     }
 }
 
