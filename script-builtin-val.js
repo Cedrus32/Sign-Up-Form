74439e7929df5b4da4e14836e33e68c707a8b4fc
Shannon <srosenb@protonmail.ch>
2022-03-05 13:50:06 -0600

    Add live name validation on `keyup` event

---
+++ script-builtin-val.js
@@ -0,0 +1,56 @@
+// -------------- //
+// CHECK FUNCTION //
+// -------------- //
+
+let firstName = document.getElementById('first-name');
+let lastName = document.getElementById('last-name');
+let firstNameError = document.getElementById('first-name-error');
+let lastNameError = document.getElementById('last-name-error');
+function checkName(nameSpace, nameSpaceError) {
+    // get values
+    let name = nameSpace;
+    let nameValue = name.value;
+    const alphaRegex = /^[A-Z]([ '-]?[a-zA-Z])*$/;
+    let nameTest = alphaRegex.test(nameValue);
+    let nameError = nameSpaceError;
+
+    //clear previous error messages
+    name.classList = '';
+    nameError.textContent = '';
+
+    // check value
+    if (nameValue.length <= 30) {
+        if (nameValue === '') {
+            name.classList.add('error');
+            if (nameSpace.id === 'first-name') {
+                nameError.textContent = '* Please Enter Your First Name';
+            } else if (nameSpace.id === 'last-name') {
+                nameError.textContent = '* Please Enter Your Last Name';
+            }
+        } else if (nameTest === false) {
+            name.classList.add('error');
+            nameError.textContent = "* Name can include letters, spaces, and the special characters  ' -";
+        } else if (nameTest === true) {
+            name.classList.add('correct');
+        }
+    } else if (nameValue.length > 30) {
+        name.classList.add('error');
+        nameError.textContent = '* Exceeds Maximum Character Count'
+    }
+}
+
+// --------- //
+// RUN CHECK //
+// --------- //
+
+firstName.addEventListener('focus', () => {
+    firstName.addEventListener('keyup', (e) => {
+        console.log(e);
+        console.log(firstName.value);
+        checkName(firstName, firstNameError);
+})});
+
+lastName.addEventListener('focus', () => {
+    lastName.addEventListener('keyup', () => {
+        checkName(lastName, lastNameError);
+})});
