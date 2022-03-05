41c9edc5928374c8d37603f67f8e330d421e75fb
Shannon <srosenb@protonmail.ch>
2022-03-05 16:05:44 -0600

    Tweak name validation

--- script-builtin-val.js
+++ script-builtin-val.js
@@ -6,38 +6,24 @@ let firstName = document.getElementById('first-name');
 let lastName = document.getElementById('last-name');
 let firstNameError = document.getElementById('first-name-error');
 let lastNameError = document.getElementById('last-name-error');
-// function checkName(nameSpace, nameSpaceError) {
-//     // get values
-//     let name = nameSpace;
-//     let nameValue = name.value;
-//     const alphaRegex = /^[A-Z]([ '-]?[a-zA-Z])*$/;
-//     let nameTest = alphaRegex.test(nameValue);
-//     let nameError = nameSpaceError;
+function checkName(nameSpace, errorSpace) {
+    // get valid
+    let isValid = nameSpace.checkValidity();
 
-//     //clear previous error messages
-//     name.classList = '';
-//     nameError.textContent = '';
+    // clear previous error messages
+    nameSpace.classList = '';
+    errorSpace.textContent = '';
 
-//     // check value
-//     if (nameValue.length <= 30) {
-//         if (nameValue === '') {
-//             name.classList.add('error');
-//             if (nameSpace.id === 'first-name') {
-//                 nameError.textContent = '* Please Enter Your First Name';
-//             } else if (nameSpace.id === 'last-name') {
-//                 nameError.textContent = '* Please Enter Your Last Name';
-//             }
-//         } else if (nameTest === false) {
-//             name.classList.add('error');
-//             nameError.textContent = "* Name can include letters, spaces, and the special characters  ' -";
-//         } else if (nameTest === true) {
-//             name.classList.add('correct');
-//         }
-//     } else if (nameValue.length > 30) {
-//         name.classList.add('error');
-//         nameError.textContent = '* Exceeds Maximum Character Count'
-//     }
-// }
+    // check value
+    if (isValid === false) {
+        errorSpace.textContent = "* Name can include letters, spaces, and the special characters  ' -";
+    } else if ((isValid === true) && (nameSpace.value !== '')) {
+        nameSpace.classList.add('correct');
+    } else if (nameSpace.value === '') {
+        nameSpace.classList = '';
+        errorSpace.textContent = '';
+    }
+}
 
 // --------- //
 // RUN CHECK //
@@ -45,16 +31,10 @@ let lastNameError = document.getElementById('last-name-error');
 
 firstName.addEventListener('focus', () => {
     firstName.addEventListener('keyup', (e) => {
-        console.log(e);
-        console.log(firstName.value);
-        console.log({ValidityState});
-        // checkName(firstName, firstNameError);
+        checkName(firstName, firstNameError)
 })});
 
 lastName.addEventListener('focus', () => {
     lastName.addEventListener('keyup', (e) => {
-        console.log(e);
-        console.log(firstName.value);
-        console.log({ValidityState});
-        // checkName(lastName, lastNameError);
+        checkName(lastName, lastNameError)
 })});
