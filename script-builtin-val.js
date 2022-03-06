72fa61c7c785a49153f9b657479b7b38ede26fd6
Shannon <srosenb@protonmail.ch>
2022-03-05 22:12:34 -0600

    Tweak email validation

--- script-builtin-val.js
+++ script-builtin-val.js
@@ -1,31 +1,30 @@
-// -------------- //
-// CHECK FUNCTION //
-// -------------- //
+// --------------- //
+// CHECK FUNCTIONS //
+// --------------- //
+
+// ---- NAME ---- //
 
 let firstName = document.getElementById('first-name');
 let lastName = document.getElementById('last-name');
 function checkName(nameSpace) {
-    // set valid
+    // set values
     let isValid = nameSpace.checkValidity();
-    let nameClass;
     let nameLabel;
     let nameError;
     if (nameSpace.id === 'first-name') {
-        nameClass = 'first-name';
         nameLabel = document.querySelector('label.first-name.required');
         nameError = document.getElementById('first-name-error');
     } else if (nameSpace.id == 'last-name') {
-        nameClass = 'last-name';
         nameLabel = document.querySelector('label.last-name.required');
         nameError = document.getElementById('last-name-error');
     }
 
-    // clear previous error messages
-    nameSpace.classList = '';
+    // clear previous error message
     nameLabel.classList.remove('hide');
+    nameSpace.classList = '';
     nameError.textContent = '';
 
-    // check value
+    // check valus
     if ((isValid === false) && (nameSpace.value !== '')) {
         nameError.textContent = "* Name can include letters, spaces, and the special characters  ' -";
     } else if ((isValid === true) && (nameSpace.value !== '')) {
@@ -34,14 +33,41 @@ function checkName(nameSpace) {
     }
 }
 
-// --------- //
-// RUN CHECK //
-// --------- //
+// ---- EMAIL ---- //
+
+let email = document.getElementById('email');
+let emailError = document.getElementById('email-error');
+function checkEmail() {
+    // set values
+    let isValid = email.checkValidity();
+    let emailLabel = document.querySelector('label.email.required');
+    let emailError = document.getElementById('email');
+
+    // clear previous error message
+    email.classList = '';
+    emailLabel.classList.remove('hide');
+    emailError.textContent = '';
+
+    // check valus
+    if ((isValid === false) ** (email.value !== '')) {
+        emailError.textContent = '* Please enter valid email address';
+    } else if ((isValid === true) && (email.value !== '')) {
+        emailLabel.classList.add('hide');
+        email.classList.add('correct');
+    }
+}
+
+// --------------- //
+// LISTENER EVENTS //
+// --------------- //
+
+// ---- NAME ---- //
 
 firstName.addEventListener('focus', () => {
     firstName.addEventListener('keyup', () => {
         checkName(firstName);
-})});
+    });
+});
 
 firstName.addEventListener('blur', () => {
     firstName.classList = '';
@@ -50,8 +76,27 @@ firstName.addEventListener('blur', () => {
 lastName.addEventListener('focus', () => {
     lastName.addEventListener('keyup', () => {
         checkName(lastName);
-})});
+    });
+});
 
 lastName.addEventListener('blur', () => {
     lastName.classList = '';
+});
+
+// ---- CONTACT ---- //
+
+email.addEventListener('focus', () => {
+    email.addEventListener('keyup', () => {
+        checkEmail();
+    });
+});
+
+email.addEventListener('blur', () => {
+    email.classList = '';
+});
+
+phone.addEventListener('focus', () => {
+    phone.addEventListener('keyup', () => {
+        checkPhone();
+    });
 });
