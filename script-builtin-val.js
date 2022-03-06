16c7243eed11dc59e8cfce11699fb69f1c42470d
Shannon <srosenb@protonmail.ch>
2022-03-05 21:39:44 -0600

    Tweak name validation styling and validation -- required badge

--- script-builtin-val.js
+++ script-builtin-val.js
@@ -4,20 +4,32 @@
 
 let firstName = document.getElementById('first-name');
 let lastName = document.getElementById('last-name');
-let firstNameError = document.getElementById('first-name-error');
-let lastNameError = document.getElementById('last-name-error');
-function checkName(nameSpace, errorSpace) {
-    // get valid
+function checkName(nameSpace) {
+    // set valid
     let isValid = nameSpace.checkValidity();
+    let nameClass;
+    let nameLabel;
+    let nameError;
+    if (nameSpace.id === 'first-name') {
+        nameClass = 'first-name';
+        nameLabel = document.querySelector('label.first-name.required');
+        nameError = document.getElementById('first-name-error');
+    } else if (nameSpace.id == 'last-name') {
+        nameClass = 'last-name';
+        nameLabel = document.querySelector('label.last-name.required');
+        nameError = document.getElementById('last-name-error');
+    }
 
     // clear previous error messages
     nameSpace.classList = '';
-    errorSpace.textContent = '';
+    nameLabel.classList.remove('hide');
+    nameError.textContent = '';
 
     // check value
     if ((isValid === false) && (nameSpace.value !== '')) {
-        errorSpace.textContent = "* Name can include letters, spaces, and the special characters  ' -";
+        nameError.textContent = "* Name can include letters, spaces, and the special characters  ' -";
     } else if ((isValid === true) && (nameSpace.value !== '')) {
+        nameLabel.classList.add('hide');
         nameSpace.classList.add('correct');
     }
 }
@@ -28,10 +40,18 @@ function checkName(nameSpace, errorSpace) {
 
 firstName.addEventListener('focus', () => {
     firstName.addEventListener('keyup', () => {
-        checkName(firstName, firstNameError)
+        checkName(firstName);
 })});
+
+firstName.addEventListener('blur', () => {
+    firstName.classList = '';
+});
 
 lastName.addEventListener('focus', () => {
     lastName.addEventListener('keyup', () => {
-        checkName(lastName, lastNameError)
+        checkName(lastName);
 })});
+
+lastName.addEventListener('blur', () => {
+    lastName.classList = '';
+});
